6ab3fcdbe387ba566619a28385f9421b95691c7b
zuna <tales.of.zuna@gmail.com>
2024-03-27 19:00:25 +0800

    ziak

--- src/themes/fennec/focused-player/name-and-stats/data-row/stats/stat/stat.js
+++ src/themes/fennec/focused-player/name-and-stats/data-row/stats/stat/stat.js
@@ -1,35 +1,41 @@
-import { teamColorClass } from '/hud/helpers/team-color-class.js'
+import { teamColorClass } from "/hud/helpers/team-color-class.js";
 
 export default {
-	props: [
-		'type',
-	],
+  props: ["type"],
 
-	computed: {
-		player() {
-			return this.$players.focused
-		},
+  computed: {
+    player() {
+      return this.$players.focused;
+    },
 
-		colorClass() {
-			return teamColorClass(this.player.team)
-		},
+    colorClass() {
+      return teamColorClass(this.player.team);
+    },
 
-		value() {
-			switch (this.type) {
-				case 'kills': return this.player.kills
-				case 'assists': return this.player.assists
-				case 'deaths': return this.player.deaths
-				case 'adr': return this.player.adr
-			}
-		},
+    value() {
+      switch (this.type) {
+        case "kills":
+          return this.player.kills;
+        case "assists":
+          return this.player.assists;
+        case "deaths":
+          return this.player.deaths;
+        case "adr":
+          return this.player.adr;
+      }
+    },
 
-		label() {
-			switch (this.type) {
-				case 'kills': return 'K'
-				case 'assists': return 'A'
-				case 'deaths': return 'D'
-				case 'adr': return 'ADR'
-			}
-		},
-	},
-}
+    label() {
+      switch (this.type) {
+        case "kills":
+          return "K";
+        case "assists":
+          return "A";
+        case "deaths":
+          return "D";
+        case "adr":
+          return "ADR";
+      }
+    },
+  },
+};
--- src/themes/fennec/radar/bomb/bomb.js
+++ src/themes/fennec/radar/bomb/bomb.js
@@ -1,9 +1,9 @@
-import DroppedBomb from '/hud/radar/bomb/dropped-bomb/dropped-bomb.vue'
-import PlantedBomb from '/hud/radar/bomb/planted-bomb/planted-bomb.vue'
+import DroppedBomb from "/hud/radar/bomb/dropped-bomb/dropped-bomb.vue";
+import PlantedBomb from "/hud/radar/bomb/planted-bomb/planted-bomb.vue";
 
 export default {
-	components: {
-		DroppedBomb,
-		PlantedBomb,
-	},
-}
+  components: {
+    DroppedBomb,
+    PlantedBomb,
+  },
+};
